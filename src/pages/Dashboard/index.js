6e4b7d26f6389c3b230aec2c3a02ac27e41715ef
rodrigorgtic <rodrigo.rgtic@gmail.com>
2019-11-12 17:22:19 -0300

    styled: appointment list

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,12 +2,27 @@ import React from 'react';
 import { View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import { Container, Title, List } from './styles';
 
 import Background from '~/components/Background';
+import Appointment from '~/components/Appointment';
+
+const data = [1, 2, 3, 4, 5];
 
 export default function Dashboard() {
-  return <Background />;
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+
+        <List
+          data={data}
+          keyExtractor={item => String(item)}
+          renderItem={({ item }) => <Appointment data={item} />}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 Dashboard.navigationOptions = {
