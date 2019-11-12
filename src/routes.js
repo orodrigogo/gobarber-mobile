6cda10d5f02d333bae048c49b22d955aa95f8fbe
rodrigorgtic <rodrigo.rgtic@gmail.com>
2019-11-12 14:53:51 -0300

    feature: protected routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,22 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
 
-export default createAppContainer(
-  createSwitchNavigator({
-    SignIn,
-    SignUp,
-  })
-);
+export default (signIn = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+        App: createBottomTabNavigator({
+          Dashboard,
+        }),
+      },
+      { initialRouteName: signIn ? 'App' : 'Sign' }
+    )
+  );
