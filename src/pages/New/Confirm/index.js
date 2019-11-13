263a79f55d62e7d180e2793561aaa6b92d1581ce
rodrigorgtic <rodrigo.rgtic@gmail.com>
2019-11-13 13:35:13 -0300

    feature: add appointment

--- src/pages/New/Confirm/index.js
+++ src/pages/New/Confirm/index.js
@@ -1,8 +1,61 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { formatRelative, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
-// import { Container } from './styles';
+import { Container, Avatar, Name, Time, SubmitButton } from './styles';
 
-export default function Confirm() {
-  return <View />;
+import Background from '~/components/Background';
+import api from '~/services/api';
+
+export default function Confirm({ navigation }) {
+  // Pegando os parametros passados para a tela.
+  const provider = navigation.getParam('provider');
+  const time = navigation.getParam('time');
+
+  const dateFormatted = useMemo(
+    () => formatRelative(parseISO(time), new Date(), { locale: pt }),
+    [time]
+  );
+
+  async function handleAddAppointment() {
+    await api.post('appointments', { provider_id: provider.id, date: time });
+
+    navigation.navigate('Dashboard');
+  }
+  return (
+    <Background>
+      <Container>
+        <Avatar
+          source={{
+            uri: provider.avatar
+              ? provider.avatar.url
+              : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+          }}
+        />
+
+        <Name>{provider.name}</Name>
+
+        <Time>{dateFormatted}</Time>
+
+        <SubmitButton onPress={handleAddAppointment}>
+          Confirmar agendamento
+        </SubmitButton>
+      </Container>
+    </Background>
+  );
 }
+
+Confirm.navigationOptions = ({ navigation }) => ({
+  title: 'Confirmar Agendamento',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#FFF" />
+    </TouchableOpacity>
+  ),
+});
