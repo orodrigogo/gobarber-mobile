67ca3c29ca7d02a8d6fe67a0dea82c5d5a89fc16
rodrigorgtic <rodrigo.rgtic@gmail.com>
2019-11-13 07:29:59 -0300

    feature: selection provide and hour

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,18 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
 import DateInput from '~/components/DateInput';
 
-import { Container } from './styles';
+import { Container, HourList, Hour, Title } from './styles';
+
+import api from '~/services/api';
 
 export default function SelectDateTime({ navigation }) {
   const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
+
+  const provider = navigation.getParam('provider');
+
+  useEffect(() => {
+    async function loadAvailable() {
+      const response = await api.get(`providers/${provider.id}/available`, {
+        params: {
+          date: date.getTime(), // retorna a data no formato de timestamp.
+        },
+      });
+
+      setHours(response.data);
+    }
+
+    loadAvailable();
+  }, [date, provider.id]);
+
+  function handleSelectHour(time) {
+    navigation.navigate('Confirm', { provider, time }); // passar dados para a proxima tela.
+  }
+
   return (
     <Background>
       <Container>
         <DateInput date={date} onChange={setDate} />
+
+        <HourList
+          data={hours}
+          keyExtractor={item => item.time}
+          renderItem={({ item }) => (
+            <Hour
+              enabled={item.available}
+              onPress={() => handleSelectHour(item.value)}
+            >
+              <Title>{item.time}</Title>
+            </Hour>
+          )}
+        />
       </Container>
     </Background>
   );
--- src/pages/New/SelectDateTime/styles.js
+++ src/pages/New/SelectDateTime/styles.js
@@ -1,5 +1,29 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.SafeAreaView`
   flex: 1;
+`;
+
+export const HourList = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+  numColumns: 2,
+})`
+  padding: 0 20px;
+`;
+
+export const Hour = styled(RectButton)`
+  flex: 1;
+  background: #fff;
+  border-radius: 4px;
+  padding: 20px;
+  opacity: ${props => (props.enabled ? 1 : 0.6)};
+  align-items: center;
+  margin: 0 10px 20px;
+`;
+
+export const Title = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #333;
 `;
