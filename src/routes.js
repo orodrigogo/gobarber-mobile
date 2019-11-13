5f978a7d111616c8f9a54065e896f494be601461
rodrigorgtic <rodrigo.rgtic@gmail.com>
2019-11-12 21:13:23 -0300

    feature: routes new appointment

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
+
+import SelectProvider from './pages/New/SelectProvider';
+import SelectDateTime from './pages/New/SelectDateTime';
+import Confirm from './pages/New/Confirm';
 
 export default (signIn = false) =>
   createAppContainer(
@@ -17,6 +25,32 @@ export default (signIn = false) =>
         App: createBottomTabNavigator(
           {
             Dashboard,
+            New: {
+              screen: createStackNavigator({
+                SelectProvider,
+                SelectDateTime,
+                Confirm,
+              }, {
+                defaultNavigationOptions: {
+                  headerTransparent: true,
+                  headerTintColor: '#FFF',
+                  headerLeftContainerStyle: {
+                    marginLeft: 20,
+                  }
+                }
+              }),
+              navigationOptions: {
+                tabBarVisible: false,
+                tabBarLabel: 'Agendar',
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255,255,255,0.6)"
+                  />
+                ),
+              },
+            },
             Profile,
           },
           {
